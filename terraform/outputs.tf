6d00cd5c3923e69d8a738df4aac6146f37f43628
Daniel van der Merwe <daniel.van.der.merwe@evolutiops.com>
2023-12-18 15:48:31 -0500

    Remove delegate token output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "delegate_token" {
-  description = "The delegate token for the Harness Delegate"
-  value       = harness_platform_delegatetoken.this.value
-}
+#output "delegate_token" {
+#  description = "The delegate token for the Harness Delegate"
+#  value       = harness_platform_delegatetoken.this.value
+#}
