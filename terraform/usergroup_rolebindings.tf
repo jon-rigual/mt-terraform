4a5467e505f710efbdb7c59fd4ce9c395de58cf5
Daniel van der Merwe <daniel.van.der.merwe@evolutiops.com>
2023-12-18 16:15:01 -0500

    Remove project and org identifier on role assignments

--- terraform/usergroup_rolebindings.tf
+++ terraform/usergroup_rolebindings.tf
@@ -1,6 +1,4 @@
-resource "harness_platform_role_assignments" "example1" {
-  org_id      = harness_platform_organization.this.id
-  project_id  = harness_platform_project.this.id
+resource "harness_platform_role_assignments" "this" {
   resource_group_identifier = harness_platform_resource_group.this.id
   role_identifier           = harness_platform_roles.this.id
   principal {
