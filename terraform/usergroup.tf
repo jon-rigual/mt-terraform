dcf25b08d5254aa3e9c33d62fe317addd2c3cde8
Daniel van der Merwe <daniel.van.der.merwe@evolutiops.com>
2023-12-18 15:50:24 -0500

    Remove linked sso id

--- terraform/usergroup.tf
+++ terraform/usergroup.tf
@@ -3,7 +3,7 @@ resource "harness_platform_usergroup" "this" {
   name        = lower(replace(join("_", [var.organization, var.project, "pipeline_creator"],), "[^a-zA-Z0-9_]", ""))
   description = "An example user group managed by Terraform."
   tags        = concat(var.default_tags, ["Demo:true"])
-  linked_sso_id      = "linked_sso_id" #Fixme
+  #linked_sso_id      = "linked_sso_id" fixme
   org_id      = harness_platform_organization.this.id
   project_id  = harness_platform_project.this.id
   # user_emails = ["i3lfrg+harness_non_admin@gmail.com"]
