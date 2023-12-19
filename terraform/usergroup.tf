5858c9fab8fe059cbc4042ab202e9a3aa0f22af4
Jeremiah <jeremiah@harness.io>
2023-12-19 10:33:26 -0500

    chore: adjust whitespace formatting

--- terraform/usergroup.tf
+++ terraform/usergroup.tf
@@ -4,8 +4,8 @@ resource "harness_platform_usergroup" "this" {
   description = "An example user group managed by Terraform."
   tags        = concat(var.default_tags, ["Demo:true"])
   #linked_sso_id      = "linked_sso_id" fixme
-#  org_id      = harness_platform_organization.this.id
-#  project_id  = harness_platform_project.this.id
+  #  org_id      = harness_platform_organization.this.id
+  #  project_id  = harness_platform_project.this.id
   # user_emails = ["i3lfrg+harness_non_admin@gmail.com"]
 }
 
