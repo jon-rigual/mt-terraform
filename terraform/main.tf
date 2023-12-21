6079f26e31a37ef0087f1970875feefaa7b24383
Jeremiah <jeremiah@harness.io>
2023-12-20 21:02:03 -0600

    feat: initial admin and non-admin resouce config

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,16 +35,49 @@ module "project" {
   depends_on = [module.organization]
 }
 
-module "resource_group" {
+module "resource_group_admin" {
   source = "./modules/resource_group"
-
-  # for_each = var.structure.projects
 
   harness_account_id = var.harness_account_id
   default_tags       = var.default_tags
   organization_name  = var.structure.organization
-  # project            = each.value
-  isAdmin = true
+  isAdmin            = true
+
+  depends_on = [module.organization, module.project]
+}
+
+module "usergroup_admin" {
+  source = "./modules/usergroup"
+
+  default_tags      = var.default_tags
+  organization_name = var.structure.organization
+  usergroup         = join("_", [var.structure.organization, "admin"])
+  isAdmin           = true
+
+  depends_on = [module.organization, module.project, module.resource_group]
+}
+
+module "usergroup_rolebindings_admin" {
+  source = "./modules/usergroup_rolebindings"
+
+  default_tags      = var.default_tags
+  organization_name = var.structure.organization
+  role              = harness_platform_roles.admin.id
+  usergroup         = join("_", [var.structure.organization, "admin"])
+  isAdmin           = true
+
+  depends_on = [module.organization, module.project, module.resource_group, module.usergroup]
+}
+
+module "resource_group" {
+  source = "./modules/resource_group"
+
+  for_each = var.structure.projects
+
+  harness_account_id = var.harness_account_id
+  default_tags       = var.default_tags
+  organization_name  = var.structure.organization
+  project            = each.value
 
   depends_on = [module.organization, module.project]
 }
@@ -52,14 +85,12 @@ module "resource_group" {
 module "usergroup" {
   source = "./modules/usergroup"
 
-  # for_each = var.structure.projects
+  for_each = var.structure.projects
 
   default_tags      = var.default_tags
   organization_name = var.structure.organization
-  # project           = each.value
-  # usergroup         = join("_", [var.structure.organization, each.value, "admin"])
-  usergroup = join("_", [var.structure.organization, "admin"])
-  isAdmin   = true
+  project           = each.value
+  usergroup         = join("_", [var.structure.organization, each.value, "admin"])
 
   depends_on = [module.organization, module.project, module.resource_group]
 }
@@ -67,15 +98,13 @@ module "usergroup" {
 module "usergroup_rolebindings" {
   source = "./modules/usergroup_rolebindings"
 
-  # for_each = var.structure.projects
+  for_each = var.structure.projects
 
   default_tags      = var.default_tags
   organization_name = var.structure.organization
-  # project           = each.value
-  role = harness_platform_roles.admin.id
-  # usergroup = join("_", [var.structure.organization, each.value, "admin"])
-  usergroup = join("_", [var.structure.organization, "admin"])
-  isAdmin   = true
+  project           = each.value
+  role              = harness_platform_roles.admin.id
+  usergroup         = join("_", [var.structure.organization, each.value, "admin"])
 
   depends_on = [module.organization, module.project, module.resource_group, module.usergroup]
 }
